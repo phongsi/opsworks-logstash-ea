677195735e97b0539519a83271a12dc58faa6d6e
Phong Si <phong.si@gmail.com>
2014-11-12 18:20:36 -0500

    revert kibana

--- opsworks-kibana/recipes/default.rb
+++ opsworks-kibana/recipes/default.rb
@@ -7,7 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe "kibana::install"
+include_recipe "kibana::default"
 
 if node['opsworks-kibana']['web_auth_enabled']
   include_recipe 'htpasswd'
