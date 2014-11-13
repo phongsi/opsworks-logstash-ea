76ae9582d4bb4feaedd4183a018aa18968a51b19
Phong Si <phong.si@gmail.com>
2014-11-13 16:26:32 -0500

    try not reloading nginx

--- opsworks-kibana/recipes/default.rb
+++ opsworks-kibana/recipes/default.rb
@@ -13,9 +13,9 @@ if node['opsworks-kibana']['web_auth_enabled']
   include_recipe 'htpasswd'
   include_recipe 'nginx'
 
-  htpasswd "/etc/nginx/htpasswd" do
-    user node['opsworks-kibana']['web_user']
-    password node['opsworks-kibana']['web_password']
-    notifies :reload, "service[nginx]"
-  end
+  # htpasswd "/etc/nginx/htpasswd" do
+  #   user node['opsworks-kibana']['web_user']
+  #   password node['opsworks-kibana']['web_password']
+  #   notifies :reload, "service[nginx]"
+  # end
 end
